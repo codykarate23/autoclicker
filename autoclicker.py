62604ba63b230e60aa4a1cf3370cbc9d078b4ec2
codykarate23 <74667781+codykarate23@users.noreply.github.com>
2021-07-08 22:05:14 -0400

    Add files via upload

---
+++ autoclicker.py
@@ -0,0 +1,177 @@
+import threading 
+import pyautogui
+import time
+import keyboard
+import pynput
+from pynput import mouse
+print("executing in 5 seconds, to stop press ctrl+enter")
+time.sleep(5)
+while(keyboard.is_pressed('ctrl+enter') == False):
+    T1 = threading.Thread(target=pyautogui.click)
+    T1.start()
+    
+    T2 = threading.Thread(target=pyautogui.click)
+    T2.start()
+
+    T3 = threading.Thread(target=pyautogui.click)
+    T3.start()
+ 
+    T4 = threading.Thread(target=pyautogui.click)
+    T4.start()
+
+    T5 = threading.Thread(target=pyautogui.click)
+    T5.start()
+
+    T6 = threading.Thread(target=pyautogui.click)
+    T6.start()
+    
+    T7 = threading.Thread(target=pyautogui.click)
+    T7.start()
+
+    T8 = threading.Thread(target=pyautogui.click)
+    T8.start()
+
+    T9 = threading.Thread(target=pyautogui.click)
+    T9.start()
+    
+    T10 = threading.Thread(target=pyautogui.click)
+    T10.start()
+
+    T11 = threading.Thread(target=pyautogui.click)
+    T11.start()
+    
+    T12 = threading.Thread(target=pyautogui.click)
+    T12.start()
+
+    T13 = threading.Thread(target=pyautogui.click)
+    T13.start()
+ 
+    T14 = threading.Thread(target=pyautogui.click)
+    T14.start()
+
+    T15 = threading.Thread(target=pyautogui.click)
+    T15.start()
+
+    T16 = threading.Thread(target=pyautogui.click)
+    T16.start()
+    
+    T17 = threading.Thread(target=pyautogui.click)
+    T17.start()
+
+    T18 = threading.Thread(target=pyautogui.click)
+    T18.start()
+
+    T19 = threading.Thread(target=pyautogui.click)
+    T19.start()
+    
+    T20 = threading.Thread(target=pyautogui.click)
+    T20.start()
+
+    T21 = threading.Thread(target=pyautogui.click)
+    T21.start()
+    
+    T22 = threading.Thread(target=pyautogui.click)
+    T22.start()
+
+    T23 = threading.Thread(target=pyautogui.click)
+    T23.start()
+
+    T24 = threading.Thread(target=pyautogui.click)
+    T24.start()
+    
+    T25 = threading.Thread(target=pyautogui.click)
+    T25.start()
+    
+ 
+    time.sleep(0.01)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
